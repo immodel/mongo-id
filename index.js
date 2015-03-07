af274603e4bc9f753f626e3d907dfe09dfdff286
Andrew Shaffer <darawk@gmail.com>
2015-03-07 15:56:25 -0800

    define type correctly

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var objectid = require('objectid');
 
 module.exports = function(model) {
-  model
-    .caster(function(value) {
-      if(value && objectid.isValid(value))
-        value = objectid.toString(value);
-      return value;
-    })
-    .get(function(value) {
-      return value && value.toString();
-    })
-    .validator(objectid.isValid, 'ObjectID')
-    .type('ObjectID');
+  model.type('ObjectID', function(model) {
+    return model
+      .caster(function(value) {
+        if(value && objectid.isValid(value))
+          value = objectid.toString(value);
+        return value;
+      })
+      .get(function(value) {
+        return value && value.toString();
+      })
+      .validator(objectid.isValid, 'ObjectID');
+  });
 };
