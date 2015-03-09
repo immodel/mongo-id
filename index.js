769443b24418eaad2012528c910b1659925c0e60
Andrew Shaffer <darawk@gmail.com>
2015-03-09 01:23:34 -0700

    immutable data

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var objectid = require('objectid');
 
-module.exports = function(model) {
-  model.type('ObjectID', function(model) {
-    return model
+module.exports = function() {
+  this.type('ObjectID', function() {
+    return this
       .caster(function(value) {
         if(value && objectid.isValid(value))
           value = objectid.toString(value);
         return value;
       })
-      .get(function(value) {
-        return value && value.toString();
-      })
-      .validator(objectid.isValid, 'ObjectID');
+      .validator(function() {
+        return objectid.isValid(this.value);
+      }, 'ObjectID');
   });
 };
